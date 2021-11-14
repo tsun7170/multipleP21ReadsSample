de3204a5c39ff81aa377df5667a022578aa4ca9c
Tsutomu Yoshida <tsuntsunkobi@icloud.com>
2021-11-15 05:08:57 +0900

    completed multipleP21ReadsSample

---
+++ multipleP21ReadsSample/MyActivityMonitor.swift
@@ -0,0 +1,90 @@
+//
+//  MyActivityMonitor.swift
+//  swiftP21read
+//
+//  Created by Yoshida on 2021/07/10.
+//  Copyright © 2021 Tsutomu Yoshida, Minokamo, Japan. All rights reserved.
+//
+
+import Foundation
+import SwiftSDAIcore
+import SwiftAP242PDMkit
+
+//MARK: - activity monitor
+class MyActivityMonitor: ExternalReferenceLoader.ActivityMonitor {
+	
+	//MARK: ExternalReferenceLoader.ActivityMonitor overrides
+	override func startedLoading(externalReference: ExternalReferenceLoader.ExternalReference) {
+		print("\n loading file: \(externalReference.name) ", terminator: "")
+		entityCount = 0
+	}
+	override func completedLoading(externalReference: ExternalReferenceLoader.ExternalReference) {
+		print(" file: \(externalReference.name) completed loading with status(\(externalReference.status))")
+	}
+	override func identified(externalReferences: [ExternalReferenceLoader.ExternalReference], 
+													 originatedFrom upstream: ExternalReferenceLoader.ExternalReference) {
+		print(" file: \(upstream.name) contains \(externalReferences.count) external references.")
+	}
+
+	
+	
+	//MARK: P21Decode.ActivityMonitor overrides
+	override func tokenStreamDidSet(error p21Error: P21Decode.P21Error) {
+		print("error detected on token stream: \(p21Error)")
+	}
+	
+	override func parserDidSet(error p21Error: P21Decode.P21Error) {
+		print("error detected on parser: \(p21Error)")
+	}
+	
+	override func exchangeStructureDidSet(error exError: String) {
+		print("error detected on exchange structure: \(exError)")
+	}
+	
+	override func decoderDidSet(error decoderError: P21Decode.Decoder.Error) {
+		print("error detected on decoder: \(decoderError)")
+	}
+	
+	override func scannerDidDetectNewLine(lineNumber: Int) {
+		if lineNumber % 1000 == 0 			{ print("+", terminator: "") }
+		else if lineNumber % 100 == 0 	{ print(".", terminator: "") }
+	}
+	
+	var entityCount = 0
+	override func decoderResolved(entiyInstanceName: P21Decode.ExchangeStructure.EntityInstanceName) {
+		entityCount += 1
+		if entityCount % 1000 == 0 			{ print("*", terminator: "") }
+		else if entityCount % 100 == 0 	{ print(".", terminator: "") }		
+	}
+	
+	override func startedParsingHeaderSection() {
+		print("\n parsing header section: ", terminator: "")
+	}
+	
+	override func startedParsingAnchorSection() {
+		print("\n parsing anchor section: ", terminator: "")
+	}
+	
+	override func startedParsingReferenceSection() {
+		print("\n parsing reference section: ", terminator: "")
+	}
+	
+	override func startedParsingDataSection() {
+		print("\n parsing data section: ", terminator: "")
+
+	}
+	
+	override func completedParsing() {
+		print("\n completed parsing.")
+	}
+	
+	override func startedResolvingEntityInstances() {
+		print(" resolving entity instances: ", terminator: "")
+
+	}
+	
+	override func completedResolving() {
+		print("\n completed resolving.")
+	}
+
+}
--- multipleP21ReadsSample/main.swift
+++ multipleP21ReadsSample/main.swift
@@ -7,6 +7,84 @@
 //
 
 import Foundation
+import SwiftSDAIcore
+import SwiftSDAIap242
+import SwiftAP242PDMkit
 
-print("Hello, World!")
+//MARK: identify the input p21 data file
+let testDataFolder = ProcessInfo.processInfo.environment["TEST_DATA_FOLDER"]!
 
+let url = URL(fileURLWithPath: testDataFolder + "CAx STEP FILE LIBRARY/s1-c5-214/s1-c5-214.stp")
+
+
+//MARK: create output repository
+let repository = SDAISessionSchema.SdaiRepository(name: "examle", description: "example repository")
+
+//MARK: prepare the acceptable step schema list
+let schemaList: P21Decode.SchemaList = [
+	"AP242_MANAGED_MODEL_BASED_3D_ENGINEERING_MIM_LF { 1 0 10303 442 3 1 4 }": AP242_MANAGED_MODEL_BASED_3D_ENGINEERING_MIM_LF.self,
+	"AP242_MANAGED_MODEL_BASED_3D_ENGINEERING_MIM_LF { 1 0 10303 442 1 1 4 }": AP242_MANAGED_MODEL_BASED_3D_ENGINEERING_MIM_LF.self,
+	"AP203_CONFIGURATION_CONTROLLED_3D_DESIGN_OF_MECHANICAL_PARTS_AND_ASSEMBLIES_MIM_LF  { 1 0 10303 403 3 1 4}": AP242_MANAGED_MODEL_BASED_3D_ENGINEERING_MIM_LF.self,
+	"AP203_CONFIGURATION_CONTROLLED_3D_DESIGN_OF_MECHANICAL_PARTS_AND_ASSEMBLIES_MIM_LF { 1 0 10303 403 2 1 2}": AP242_MANAGED_MODEL_BASED_3D_ENGINEERING_MIM_LF.self,
+	"CONFIG_CONTROL_DESIGN": AP242_MANAGED_MODEL_BASED_3D_ENGINEERING_MIM_LF.self,
+	"CONFIGURATION_CONTROL_3D_DESIGN_ED2_MIM_LF { 1 0 10303 403 1 1 4}": AP242_MANAGED_MODEL_BASED_3D_ENGINEERING_MIM_LF.self,
+	"AUTOMOTIVE_DESIGN { 1 0 10303 214 1 1 1 1 }": AP242_MANAGED_MODEL_BASED_3D_ENGINEERING_MIM_LF.self,
+]
+
+//MARK: create external reference loader
+let monitor = MyActivityMonitor()
+guard let loader = ExternalReferenceLoader(repository: repository, 
+																		 schemaList: schemaList, 
+																		 masterFile: url, 
+																		 monitor: monitor)
+else {
+	print("failed to create ExternalReferenceLoader")
+	exit(1)
+}
+
+//MARK: decode
+loader.decode()
+
+//MARK: list loaded
+let sorted = loader.externalReferences.values.sorted { 
+	if $0.level == $1.level { return $0.name < $1.name }
+	return $0.level < $1.level
+}
+
+for (i,extref) in sorted.enumerated() {
+	print("")
+	print("[\(i)]\t LEVEL.\(extref.level)\t \(extref.name)" )
+	print("\t STATUS   = \(extref.status)")
+	print("\t UPSTREAM = \(extref.upStream?.name ?? "none")")
+	print("\t MODELS   = \(extref.sdaiModels.map{$0.name})")
+	for (j,linkage) in extref.externalShapeDefinitionLinkages.enumerated() {
+		let productId = linkage.master.DEFINITION.DEFINITION.FORMATION?.OF_PRODUCT.ID.asSwiftType ?? "(no product id)"
+		let productName = linkage.master.DEFINITION.DEFINITION.FORMATION?.OF_PRODUCT.NAME.asSwiftType ?? "(no product name)"
+		let shapeId = linkage.master.USED_REPRESENTATION.ID?.asSwiftType ?? "(no shape rep id)"
+		let shapeName = linkage.master.USED_REPRESENTATION.NAME.asSwiftType
+		
+		print("\t LINKAGE<\(j)>\t ID = '\(productId)'/'\(shapeId)'\t NAME = '\(productName)'/'\(shapeName)'")
+		print("\t\t MASTER = \(linkage.master)")
+		print("\t\t DETAIL = \(linkage.detail)")
+	}
+}
+
+//MARK: create a schema instance containing all models
+let allmodels = repository.createSchemaInstance(name: url.lastPathComponent, 
+																								schema: ap242.schemaDefinition)
+allmodels.add(models: loader.sdaiModels)
+allmodels.mode = .readOnly
+
+//MARK: validation
+var doAllValidaton = true
+if doAllValidaton {
+	let validationPassed = allmodels.validateAllConstraints(monitor: MyValidationMonitor())
+	
+	print("\n\n validationPassed: ", validationPassed)
+	print("\n glovalRuleValidationRecord: \(String(describing: allmodels.globalRuleValidationRecord))"  )
+	print("\n uniquenessRuleValidationRecord: \(String(describing: allmodels.uniquenessRuleValidationRecord))")
+	print("\n whereRuleValidationRecord: \(String(describing: allmodels.whereRuleValidationRecord))" )
+	print("")
+}
+
+print("")
